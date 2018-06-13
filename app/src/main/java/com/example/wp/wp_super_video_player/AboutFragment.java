f4be9f658ca6b339433f19678a0ffd1c57ade7d1
wp <258188170@qq.com>
2018-06-13 22:57:03 +0800

    关于界面

--- app/src/main/java/com/example/wp/wp_super_video_player/AboutFragment.java
+++ app/src/main/java/com/example/wp/wp_super_video_player/AboutFragment.java
@@ -2,17 +2,24 @@ package com.example.wp.wp_super_video_player;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.wp.wp_super_video_player.api.Router;
 import com.example.wp.wp_super_video_player.base.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * Created by wangpeng .
  */
 @Route(path = Router.ABOUT_FRAGMENT)
 public class AboutFragment extends BaseFragment {
+    @BindView(R.id.app_desc)
+    TextView mTextView;
     @Override
     protected Object setLayout() {
         return R.layout.fragment_about;
@@ -22,5 +29,9 @@ public class AboutFragment extends BaseFragment {
     @Override
     protected void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
 
+        //表示文字中有连接可点
+        mTextView.setAutoLinkMask(Linkify.ALL);
+        //表示文字可以滚动
+        mTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
