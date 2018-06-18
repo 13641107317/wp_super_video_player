6eff60554ab722c2409cc54322f379df5d96503d
wp <258188170@qq.com>
2018-06-18 21:24:22 +0800

    数据获取

--- app/src/main/java/com/example/wp/wp_super_video_player/adapter/DetailAdapter.java
+++ app/src/main/java/com/example/wp/wp_super_video_player/adapter/DetailAdapter.java
@@ -1,11 +1,18 @@
 package com.example.wp.wp_super_video_player.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.wp.wp_super_video_player.R;
 import com.example.wp.wp_super_video_player.entity.Albnm;
+import com.example.wp.wp_super_video_player.entity.AlbumList;
 import com.example.wp.wp_super_video_player.entity.Channel;
 
 /**
@@ -14,32 +21,75 @@ import com.example.wp.wp_super_video_player.entity.Channel;
 public class DetailAdapter extends RecyclerView.Adapter {
     private Context mContext;
     private Channel mChannel;
+    private AlbumList mAlbumList;
+    private int mColunms;
 
     public DetailAdapter(Context context, Channel channel) {
         super();
-        this.mChannel =channel;
+        this.mChannel = channel;
         this.mContext = context;
+        mAlbumList = new AlbumList();
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = ((Activity) mContext).getLayoutInflater().inflate(R.layout.item_detail_list, null);
+        ItemViewHolder viewHolder = new ItemViewHolder(view);
+        view.setTag(viewHolder);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (mAlbumList == null && mAlbumList.size() == 0) {
+            return;
+        }
+        Albnm albnm = getItem(position);
+        if (holder instanceof ItemViewHolder) {
+            ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+            itemViewHolder.tv_name.setText(albnm.getTitle());
+            if (albnm.getTip().isEmpty()) {
+                itemViewHolder.tv_tip.setVisibility(View.GONE);
+            } else {
+                itemViewHolder.tv_tip.setText(albnm.getTip());
+            }
+        }
+    }
+
+    private Albnm getItem(int position) {
+        return mAlbumList.get(position);
 
     }
 
+    //显示列数
     public void setColunms(int colunms) {
+        mColunms = colunms;
+    }
 
-    }
     public void addData(Albnm albnms) {
-        //TODO
+        mAlbumList.add(albnms);
     }
+
     @Override
     public int getItemCount() {
+        if (mAlbumList != null && mAlbumList.size() > 0) {
+            return mAlbumList.size();
+        }
         return 0;
+    }
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+        private LinearLayout resultContainer;
+        private ImageView albumPoster;
+        private TextView tv_tip, tv_name;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            resultContainer = itemView.findViewById(R.id.ll_album);
+            albumPoster = itemView.findViewById(R.id.iv_album_poster);
+            tv_name = itemView.findViewById(R.id.tv_album_name);
+            tv_tip = itemView.findViewById(R.id.tv_album_tip);
+        }
     }
 }
---
+++ app/src/main/java/com/example/wp/wp_super_video_player/utils/CustomGlideModule.java
@@ -0,0 +1,10 @@
+package com.example.wp.wp_super_video_player.utils;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by WangPeng on 2018/6/18.
+ */
+@GlideModule
+public class CustomGlideModule extends AppGlideModule {}
